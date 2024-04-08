8c07bbf21e6b344c3d0129e838adad957c5db450
spacekay <bethjkim0311@gmail.com>
2024-04-09 00:16:30 +0900

    feat: logout feature

--- pages/dday.tsx
+++ pages/dday.tsx
@@ -1,17 +1,16 @@
-import { getToken } from "next-auth/jwt";
 import { useSession } from "next-auth/react";
 
-const Dday = () =>{ 
-  
+
+const Dday = () =>{
   const { data: session } = useSession();
-  
-  return (
-    <>
-    <div className="container p-5 my-5 bg-dark text-white">
-  <h1>My First Bootstrap Page {JSON.stringify(session.user.name)}</h1>
-  <p>This container has a dark background color and a white text, and some extra padding and margins.</p>
+ // dday, alarm, logout에 세션이 없는 사람이 접근하면 /로 가도록 바꿔야 함
+ return (
+   <>
+   <div className="container p-5 my-5 bg-dark text-white">
+ <h1>My First Bootstrap Page {JSON.stringify(session)}</h1>
+ <p>This container has a dark background color and a white text, and some extra padding and margins.</p>
 </div>
-    </>
+   </>
 )};
 
 export default Dday;
--- pages/logout.tsx
+++ pages/logout.tsx
@@ -1,18 +1,11 @@
 import { signOut } from "next-auth/react";
-import { NextRequest, NextResponse } from "next/server";
+import { useEffect } from "react";
 
-const Logout = (req: NextRequest, res: NextResponse) => {
-  signOut();
-  // console.log(req);
-  // NextResponse.redirect(`${req.nextUrl.origin}/`);
-//   (
-//   <>
-//   <div className="container p-5 my-5 bg-dark text-white">
-// <h1>My First Bootstrap Page</h1>
-// <p>This container has a dark background color and a white text, and some extra padding and margins.</p>
-// </div>
-//   </>
-// )
+
+const Logout = () => {
+ useEffect(()=>{
+ signOut({ callbackUrl: '/', redirect:true });
+ });
 };
 
 export default Logout;
